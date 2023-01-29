24db3d8fda8b19650a358f001f1d76bb60ea5f43
TheAshadRaza <skills.keys@gmail.com>
2023-01-29 13:17:57 +0530

    First commit of Dtechfly project

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import Features from 'components/Features';
+import Footer from 'components/Footer';
+import HeroSection from 'components/HeroSection';
+import Testimonial from 'components/Testimonial';
+
+
+export default function Home(){
+  return (
+  <>
+  <HeroSection />
+  <Features /> 
+  <Testimonial />
+  <Footer />
+  </>
+  )
+}
