233b4f8aad7d3a94daa2261a75f9af4c33beae2e
Md Ashad Raza <skills.keys@gmail.com>
2023-07-29 20:26:04 +0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,7 +69,7 @@ export default function Navbar() {
             width ={useBreakpointValue({base:'180px', md:'200px'})}
 
               >
-                <Image src='https://freeimage.host/i/HZRndKX' alt='logo' width={'170px'} 
+                <Image src="https://freeimage.host/i/HZRndKX" alt='logo' width={'170px'} 
                   height={useBreakpointValue({base:'180px', md:'190px'})}
             />
                 </Button>
